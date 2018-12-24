e20c895e6a610a253df9547bc4553f5ac9f56e9f
Hyun Min Park <hmparki253@gmail.com>
2018-12-24 17:15:54 +0900

    input이 여러개 일때 처리

--- event-handling/src/EventPractice.js
+++ event-handling/src/EventPractice.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
 
     state = {
+        username: '',
         message: ''
     }
 
@@ -16,13 +17,14 @@ class EventPractice extends Component {
     // 바벨을 이용해 arrow function으로 재정의
     handleChange = (e) => {
         this.setState({
-            message: e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
     handleClick = () => {
-        alert(this.state.message);
+        alert(this.state.username + ': ' + this.state.message);
         this.setState({
+            username: '',
             message: ''
         });
     }
@@ -31,6 +33,13 @@ class EventPractice extends Component {
         return (
             <div>
                 <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="유저이름"
+                    value={this.state.username}
+                    onChange={this.handleChange} 
+                />
                 <input
                     type="text"
                     name="message"
