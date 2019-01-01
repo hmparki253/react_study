4fef5744dc46df9a8ab33fc2e019b1960b2c965a
Hyun Min Park <hmparki253@gmail.com>
2019-01-02 00:01:08 +0900

    make example of TodoList

---
+++ todo-list/src/components/TodoItem/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styles from './TodoItem.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class TodoItem extends Component {
+    render() {
+        // 비구조화 할당문법
+        // ex const done = this.props; const children = this.props;
+        const {done, children, onToggle, onRemove} = this.props;
+
+        return (
+            <div className={cx('todo-item')} onClick={onToggle}>
+                <input className={cx('tick')} type='checkbox' checked={done} readOnly />
+                <div className={cx('text', {done})}>{children}</div>
+                <div className={cx('delete')} onClick={onRemove}>지우기</div>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
