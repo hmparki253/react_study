5e62d9b391d287681d6c68119613edf2f33f280e
Hyun Min Park <hmparki253@gmail.com>
2019-01-01 14:14:25 +0900

    classNames study

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from './CSSModule.module.css';
+
+const cx = classNames.bind(styles);
 
 const CSSModule = () => {
     return (
-        <div className={`${styles.wrapper} ${styles.inverted}`}>            
+        <div className={cx('wrapper', 'inverted')}>
             안녕하세요 korean heroes, 
             나는 <span className="something">알란다비리이다!</span>
             {/* 여기에서 something은 global 처리를 했기 때문에 이렇게 접근하여 사용할 수 있다.*/}
